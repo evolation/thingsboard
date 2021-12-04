ef0a9dcd36700429f70917a01490091cf0f0e8d3
integreedy <hmmy.ntua@gmail.com>
2021-12-04 20:44:17 +0100

    adding files

--- dao/src/test/resources/sql/system-data.sql
+++ dao/src/test/resources/sql/system-data.sql
@@ -18,12 +18,12 @@
 
 /** System admin **/
 INSERT INTO tb_user ( id, created_time, tenant_id, customer_id, email, search_text, authority )
-VALUES ( '5a797660-4612-11e7-a919-92ebcb67fe33', 1592576748000, '13814000-1dd2-11b2-8080-808080808080', '13814000-1dd2-11b2-8080-808080808080', 'sysadmin@thingsboard.org',
-         'sysadmin@thingsboard.org', 'SYS_ADMIN' );
+VALUES ( '5a797660-4612-11e7-a919-92ebcb67fe33', 1592576748000, '13814000-1dd2-11b2-8080-808080808080', '13814000-1dd2-11b2-8080-808080808080', 'sysadmin@pilones.de',
+         'sysadmin@pilones.de', 'SYS_ADMIN' );
 
 INSERT INTO user_credentials ( id, created_time, user_id, enabled, password )
 VALUES ( '61441950-4612-11e7-a919-92ebcb67fe33', 1592576748000, '5a797660-4612-11e7-a919-92ebcb67fe33', true,
-         '$2a$10$5JTB8/hxWc9WAy62nCGSxeefl3KWmipA9nFpVdDa0/xfIseeBB4Bu' );
+         '$2a$10$ZRtooXRrf1BBRK5.byOydO5pPeVqu3hGeJVrQwhrZUg3RE4c/c05u' );
 
 /** System settings **/
 INSERT INTO admin_settings ( id, created_time, key, json_value )
